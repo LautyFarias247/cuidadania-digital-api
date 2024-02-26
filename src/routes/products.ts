5140dc215a4803765128c38ccfcda2ee5cc08d69
LautyFarias247 <lauty0618@gmail.com>
2024-02-25 21:07:51 -0300

    test route

--- src/routes/products.ts
+++ src/routes/products.ts
@@ -8,5 +8,5 @@ router.post("/",createProduct)
 router.patch("/:id",updateProduct)
 router.delete("/:id",deleteProduct)
 
-
+router.get("/test",(req,res) => res.status(200).send("todook"))
 export default router
