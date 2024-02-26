00c4f3e62477c2bd16013e16cd62998aff2a6fcf
LautyFarias247 <lauty0618@gmail.com>
2024-02-25 22:21:52 -0300

    error handle on model routes

--- src/controllers/models.controller.ts
+++ src/controllers/models.controller.ts
@@ -1,30 +1,43 @@
+import { Request,Response } from "express"
 import { pool } from "../db"
 
-export const createProduct = async () => {
-	await pool.query(`CREATE TABLE Producto (
-		id INT(9) NOT NULL AUTO_INCREMENT,
-		nombre VARCHAR(255) NOT NULL,
-		descripcion VARCHAR(255) DEFAULT NULL,
-		precio DECIMAL(9,2) NOT NULL,
-		cantidad INT(7) NOT NULL,
-		PRIMARY KEY(id))`
-	)
+export const createProduct = async (_: Request,res: Response) => {
+	try {
+
+		await pool.query(`CREATE TABLE Producto (
+			id INT(9) NOT NULL AUTO_INCREMENT,
+			nombre VARCHAR(255) NOT NULL,
+			descripcion VARCHAR(255) DEFAULT NULL,
+			precio DECIMAL(9,2) NOT NULL,
+			cantidad INT(7) NOT NULL,
+			PRIMARY KEY(id))`
+		)
+		return res.status(200).send("ok")
+	} catch (error) {
+		return res.status(400).send("error!")
+	}
 
 }
 
-export const createClient = async () => {
-	await pool.query(`CREATE TABLE Cliente (
-		id INT(9) NOT NULL AUTO_INCREMENT,
-		nombre VARCHAR(255) NOT NULL,
-		email VARCHAR(255) NOT NULL,
-		direccion VARCHAR(255) NOT NULL,
-		PRIMARY KEY(id)
-	)`)
+export const createClient = async (_: Request,res: Response) => {
+	try {
+		await pool.query(`CREATE TABLE Cliente (
+			id INT(9) NOT NULL AUTO_INCREMENT,
+			nombre VARCHAR(255) NOT NULL,
+			email VARCHAR(255) NOT NULL,
+			direccion VARCHAR(255) NOT NULL,
+			PRIMARY KEY(id)
+		)`)
+		return res.status(200).send("ok")
+	} catch (error) {
+		return res.status(400).send("error!")
+	}
 }
 
-export const createSale = async () => {
+export const createSale = async (_: Request,res: Response) => {
+	try {
 
-	await pool.query(`CREATE TABLE Venta (
+		await pool.query(`CREATE TABLE Venta (
 		id INT(9) NOT NULL AUTO_INCREMENT,
 		fecha DATETIME NOT NULL,
 		id_cliente INT(9),
@@ -32,13 +45,19 @@ export const createSale = async () => {
 		estado VARCHAR(20) CHECK (estado IN ('entregado', 'pagado', 'en proceso')),
 		PRIMARY KEY(id),
 		FOREIGN KEY (id_cliente) REFERENCES Cliente(id)
-	
-	)`)
+		
+		)`)
+		return res.status(200).send("ok")
+
+	} catch (error) {
+		return res.status(400).send("error!")
+	}
 }
 
-export const createSaleDetail = async () => {
+export const createSaleDetail = async (_: Request,res: Response) => {
+	try {
 
-	await pool.query(`CREATE TABLE DetalleVenta (
+		await pool.query(`CREATE TABLE DetalleVenta (
 		id INT(9) NOT NULL AUTO_INCREMENT,
 		id_venta INT(9),
 		id_producto INT(9),
@@ -47,5 +66,10 @@ export const createSaleDetail = async () => {
 		PRIMARY KEY(id),
 		FOREIGN KEY (id_venta) REFERENCES Venta(id),
 		FOREIGN KEY (id_producto) REFERENCES Producto(id)
-	)`)
+		)`)
+		return res.status(200).send("ok")
+
+	} catch (error) {
+		return res.status(400).send("error!")
+	}
 }
